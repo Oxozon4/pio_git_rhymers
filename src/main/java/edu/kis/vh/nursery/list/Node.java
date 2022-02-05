127421c5b6c49df05ded2956e770498919571bcd
Oxozon <oxozon.o4@gmail.com>
2022-02-05 23:30:09 +0100

    1.4 accessory 3 | dodanie hermetyzacji nieprywatnych atrybutow

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -9,4 +9,23 @@ public class Node {
         value = i;
     }
 
+    public void setPrevious(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node getPrevious() {
+        return prev;
+    }
 }
