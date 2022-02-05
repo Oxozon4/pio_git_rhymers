2e1f66e01e92c4d35d964c55683eac87e3bec424
Oxozon <oxozon.o4@gmail.com>
2022-02-06 00:40:09 +0100

    1.5.6 | Dodanie szkieletu dokumentacji do klasy HanoiRhymer i jej metod

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,30 @@
 package edu.kis.vh.nursery;
 
+/**
+ * HanoiRhymer class that extends DefaultCountingOutRhymer 
+ * @see HanoiRhymer
+ * @see DefaultCountingOutRhymer
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
 
+    /**
+     * public getter function to get totalRejected variable value
+     * @return value of totalRejected variable
+     * @see HanoiRhymer
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * public countIn function to increment totalRejected variable or call countIn method
+     * from DefaultCountingOutRhymer class
+     * @return value of totalRejected variable
+     * @see HanoiRhymer
+     * @see DefaultCountingOutRhymer
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
