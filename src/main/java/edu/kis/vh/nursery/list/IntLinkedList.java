ce26067917049e915d5664e1da0b617c06948472
Oxozon <oxozon.o4@gmail.com>
2022-02-05 23:56:28 +0100

    1.5.2 | Przeniesienie klasy Node do intLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -38,5 +38,35 @@ public class IntLinkedList {
         return ret;
     }
 
+    private static class Node {
+
+        private final int value;
+        private Node prev, next;
+    
+        public Node(int i) {
+            value = i;
+        }
+    
+        public void setPrevious(Node prev) {
+            this.prev = prev;
+        }
+    
+        public Node getNext() {
+            return next;
+        }
+    
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    
+        public int getValue() {
+            return value;
+        }
+    
+        public Node getPrevious() {
+            return prev;
+        }
+    }
+
     // 1.4 accesory 4 | Podczas wykonywania zadania nie zauwazylem zadnego nieuzywanego settera w repozytorium.
 }
